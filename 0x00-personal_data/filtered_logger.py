b27bbdff398658a054454096f5d676f8895d2edc
elpastore <abdoulayesadio1997@gmail.com>
2024-05-29 23:11:29 +0000

    task0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+filtered_logger module
+"""
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """
+     returns the log message obfuscated:
+    """
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
