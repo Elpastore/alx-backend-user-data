f64e1095f63d429663a6244b03aa2de890df9bdf
elpastore <abdoulayesadio1997@gmail.com>
2024-05-30 17:38:55 +0000

    task5

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -11,3 +11,10 @@ def hash_password(password: str) -> bytes:
     is a byte string
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    check if password is valid
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
