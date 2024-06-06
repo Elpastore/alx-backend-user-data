05c447246237de64d78ef65d09b4815421627e8c
elpastore <abdoulayesadio1997@gmail.com>
2024-06-06 16:43:18 +0000

    task4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,19 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ require_auth
         """
-        return False
+        if path is None:
+            return True
+
+        if excluded_paths is None or excluded_paths == []:
+            return True
+
+        if path in excluded_paths:
+            return False
+
+        for excluded_path in excluded_paths:
+            if excluded_path.startswith(path):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ authorization_header
