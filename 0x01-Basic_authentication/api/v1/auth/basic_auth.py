6a97400712f456d045e7d0650bf11bec26bd2804
elpastore <abdoulayesadio1997@gmail.com>
2024-06-07 15:42:52 +0000

    task10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,6 +6,7 @@ from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -60,3 +61,24 @@ class BasicAuth(Auth):
             return email, password
         else:
             return None, None
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """
+        return user instance based on email and password
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        try:
+            users = User.search({"email": user_email})
+            if users:
+                for user in users:
+                    if user.is_valid_password(user_pwd):
+                        return user
+            else:
+                return None
+        except Exception:
+            return None
