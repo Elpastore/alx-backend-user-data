1b18d0b6749f8de237c5e16157604dc63500676c
elpastore <abdoulayesadio1997@gmail.com>
2024-06-07 12:37:04 +0000

    task5

---
+++ 0x01-Basic_authentication/api/v1/auth/test.py
@@ -0,0 +1,12 @@
+dictionary = {
+    "Authorization": "Basic nnlenngnlsnslgnsg:OLNNII=="
+}
+
+dictç_2 = {
+    "Authorisation": "Basic nnlenngnlsnslgnsg:OLNNII=="
+}
+
+if "Authorization"  in dictionary.keys():
+    print("OK")
+else:
+    print("NO")
