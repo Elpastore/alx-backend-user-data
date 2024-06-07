8236623e075532be946bace37c025477850cec8d
elpastore <abdoulayesadio1997@gmail.com>
2024-06-07 13:06:22 +0000

    task7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,4 +11,18 @@ class BasicAuth(Auth):
     """
     BasicAuth class
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        return the Base64 part of the Authorization header
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        token = authorization_header.split(" ")[-1]
+        return token
