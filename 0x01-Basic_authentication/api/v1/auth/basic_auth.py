cca94fdcf00ec02e28f3d3f703adce48bd273970
elpastore <abdoulayesadio1997@gmail.com>
2024-06-07 12:47:33 +0000

    task6

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+basic_auth module
+"""
+from flask import request
+from typing import List, TypeVar
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    BasicAuth class
+    """
+    pass
