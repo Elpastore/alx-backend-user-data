2855ca2280bcfc3beacbe91e345649d91f4e3b2c
elpastore <abdoulayesadio1997@gmail.com>
2024-06-07 14:03:43 +0000

    task9

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -42,3 +42,21 @@ class BasicAuth(Auth):
                     base64_authorization_header).decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        extract users's credentials
+        return email and password
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        credentials = decoded_base64_authorization_header.split(":", 1)
+        if len(credentials) == 2:
+            email = credentials[0]
+            password = credentials[1]
+            return email, password
+        else:
+            return None, None
