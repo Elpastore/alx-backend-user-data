a12e0af297d0ec88921c217f7a374ddde0d5278c
elpastore <abdoulayesadio1997@gmail.com>
2024-06-10 17:00:03 +0000

    task4

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+auth module
+"""
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """
+    method that hash a given password
+    """
+    salt =  bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode(), salt)
+    return hashed_password
