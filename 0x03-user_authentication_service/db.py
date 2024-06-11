84e3fa9645eb2208ec6b8ba1616062ef34fb5dd9
elpastore <abdoulayesadio1997@gmail.com>
2024-06-11 12:38:11 +0000

    test

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,8 +5,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+
 from user import Base, User
 
 
@@ -17,7 +18,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -32,15 +33,19 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """add a user
+        """_summary_
         """
         new_user = User(email=email, hashed_password=hashed_password)
+        # add new user and commit to database
         self._session.add(new_user)
         self._session.commit()
         return new_user
 
     def find_user_by(self, **kwargs) -> User:
-        """find a user
+        """_summary_
+
+        Returns:
+            User: _description_
         """
         if not kwargs:
             raise InvalidRequestError
@@ -51,14 +56,16 @@ class DB:
         return user
 
     def update_user(self, user_id: int, **kwargs) -> None:
+        """_summary_
+
+        Args:
+            user_id (int): _description_
         """
-        update user credentials method
-        """
-        session = self._session
         user = self.find_user_by(id=user_id)
         for key, value in kwargs.items():
-            if hasattr(user, key):
-                setattr(user, key, value)
-            else:
+            if not hasattr(user, key):
                 raise ValueError
-        session.commit()
+            setattr(user, key, value)
+
+        self._session.commit()
+        return None
