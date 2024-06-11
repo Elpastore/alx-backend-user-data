ac95ac84af0893088252f9dc0a98374e4d88c55d
elpastore <abdoulayesadio1997@gmail.com>
2024-06-11 14:35:50 +0000

    task6

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -10,8 +10,14 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"],strict_slashes=False)
 def home() -> str:
+    """
+    welcome route
+    """
     return jsonify({"message": "Bienvenue"})
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    """
+    main app
+    """
+    app.run(host='0.0.0.0', port="5000", debug=False)
