8481880e8abe92e52942da00f297969bf10dd07e
elpastore <abdoulayesadio1997@gmail.com>
2024-06-11 15:25:54 +0000

    tassk9

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,6 +6,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
@@ -49,3 +50,9 @@ class Auth:
                                   user.hashed_password)
         except NoResultFound:
             return False
+
+    def _generate_uuid(self) -> str:
+        """
+        generate a uuid
+        """
+        return str(uuid.uuid4())
