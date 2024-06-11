06e1a1c7674a3466d8f4071642ca84f10b00b568
elpastore <abdoulayesadio1997@gmail.com>
2024-06-11 14:33:58 +0000

    task6

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -8,7 +8,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/", methods=["GET"],strict_slashes=False)
 def home() -> str:
     return jsonify({"message": "Bienvenue"})
 
